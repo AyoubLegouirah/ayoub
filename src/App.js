832e5b1778bde174b495ee9d11283ba5ad4ce03f
AyoubLegouirah <ayoubgrand@outlook.fr>
2024-01-19 22:34:28 +0100

    mise a jour2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Project from "./Components/Projects";
 import MoveToTop from "./Components/MoveToTop";
-import Lottie from  "lottie-react";
-// import nightsky from "./LottieFiles/night-sky.json";
 import HashLoader from "react-spinners/HashLoader";
 
 
